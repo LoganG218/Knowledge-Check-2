59f837c50ff724738dcfd4abfbdc444ece03645e
LoganG218 <125411469+LoganG218@users.noreply.github.com>
2023-03-12 15:20:44 -0400

    Add files via upload

---
+++ Knowledge Check 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Knowledge_Check_2;
+
+Console.WriteLine("How many String Instruments do you want to add?");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<StringInstrument>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var stringInstrument = new StringInstrument();
+
+    Console.WriteLine("Enter number of strings.");
+    stringInstrument.strings = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Is it Acoustic?");
+    stringInstrument.acoustic = Console.ReadLine() == "true";
+
+    Console.WriteLine("Enter Intrument Name");
+    stringInstrument.Name = Console.ReadLine();
+
+    recordList.Add(stringInstrument);
+}
+
+// Print out the list of records using Console.WriteLine()
+foreach (var stringInstrument in recordList) 
+{
+    Console.WriteLine(stringInstrument.Name);
+    Console.WriteLine(stringInstrument.acoustic);
+    Console.WriteLine(stringInstrument.strings);
+}
---
+++ Knowledge Check 2/Strings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Check_2
+{
+    public class Instrument
+    {
+        public string Name;
+    }
+    public class StringInstrument : Instrument 
+    {
+        public int strings;
+        public bool acoustic;
+    }
+}
